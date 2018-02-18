6cd1877360921bf620de00af36fde2f0ba4e48c9
Shantam Vijayputra <vshantam@yahoo.com>
2018-02-19 00:32:04 +0530

    Added Gender Prediction and Gender Analysis.
    Updated main function with gender predictions using CNN Neural Network..

---
+++ Gender/gender.py
@@ -0,0 +1,90 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+'''# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('training',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('test',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+history  = classifier.fit_generator(training_set,
+                         samples_per_epoch = 200,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 200)
+
+classifier.save("gclf.h5py")
+'''
+import keras.models
+clf = keras.models.load_model("gclf.h5py")
+
+import numpy as np
+from keras.preprocessing import image
+img= image.load_img("",target_size = (64,64))
+img= image.img_to_array(img)
+round(clf.predict(img[None,:,:,:])[0][0])
+
+
+import matplotlib.pyplot as plt
+
+x = history.history['loss']
+y = history.history['acc']
+
+plt.pie(x,y)
