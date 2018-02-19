b553a7b17e458a71966688610d844f346b32edee
Shantam Vijayputra <vshantam@yahoo.com>
2018-02-20 00:39:44 +0530

    added distance calculation
    Added functions which calculate the distance of object whose dimensions are known.] from the webcam.

--- main.py
+++ main.py
@@ -25,6 +25,44 @@ import keras.models
 #printing doc
 print(__doc__)
 
+def find_marker(image):
+	# convert the image to grayscale, blur it, and detect edges
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 35, 125)
+
+	# find the contours in the edged image and keep the largest one;
+	# we'll assume that this is our piece of paper in the image
+	_, cnts, _= cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	c = max(cnts, key = cv2.contourArea)
+
+	# compute the bounding box of the of the paper region and return it
+	return cv2.minAreaRect(c)
+
+def distance_to_camera(knownWidth, focalLength, perWidth):
+	# compute and return the distance from the maker to the camera
+	return (knownWidth * focalLength) / perWidth
+
+# initialize the known distance from the camera to the object, which
+# in this case is 24 inches
+KNOWN_DISTANCE = 14
+
+# initialize the known object width, which in this case, the piece of
+# paper is 12 inches wide
+KNOWN_WIDTH = 5
+
+# initialize the list of images that we'll be using
+IMAGE_PATHS = []
+
+# load the furst image that contains an object that is KNOWN TO BE 2 feet
+# from our camera, then find the paper marker in the image, and initialize
+# the focal length
+image = cv2.imread("C://Users/Shantam Vijayputra/Desktop/trainpic.jpg")
+marker = find_marker(image)
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+
 #Capturing Images.
 cap=cv2.VideoCapture(0) #For Primary webcam
 
@@ -100,6 +138,13 @@ while 1:
                         
                 #detection of eyes
                 eyes = eye_cascade.detectMultiScale(roi_gray)
+
+                marker = find_marker(roi_color)
+                
+                inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+                
+                cv2.putText(img, "%.2fft" % (inches / 12),(x , y), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 255), 1)
+
                 
                 #looping through eye coordinates
                 for (ex,ey,ew,eh) in eyes:
